0c9b05808fd6073f699a44a256574c8c3caa75fe
Alex <alexn3469@mail.ru>
2025-09-29 16:41:15 +0300

    implementing Circuit Breaker

---
+++ accounts/src/main/java/dev/alexn/service/client/CardsFallback.java
@@ -0,0 +1,16 @@
+package dev.alexn.service.client;
+
+
+import dev.alexn.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+
+}
---
+++ accounts/src/main/java/dev/alexn/service/client/LoansFallback.java
@@ -0,0 +1,14 @@
+package dev.alexn.service.client;
+
+import dev.alexn.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
